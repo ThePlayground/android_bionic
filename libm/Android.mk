7e43acc0e792e52e94628fea4da6aa7e163911b0
TwistedUmbrella <TwistedUmbrella@gmail.com>
2012-05-12 08:02:59 -0400

    Revert "MIPS support to libm, libdl and libthread_db"
    This reverts commit bc0094c7fa09f21f31cbda56b48fe1d318a69f6c.

--- libm/Android.mk
+++ libm/Android.mk
@@ -152,7 +152,6 @@ libm_common_src_files:= \
 	src/s_isnan.c \
 	src/s_modf.c
 
-libm_common_cflags :=
 
 ifeq ($(TARGET_ARCH),arm)
   libm_common_src_files += \
@@ -163,10 +162,10 @@ ifeq ($(TARGET_ARCH),arm)
 	src/s_scalbnf.c
 
   libm_common_includes = $(LOCAL_PATH)/arm
-endif
 
-ifeq ($(TARGET_OS)-$(TARGET_ARCH),linux-x86)
-  libm_common_src_files += \
+else
+  ifeq ($(TARGET_OS)-$(TARGET_ARCH),linux-x86)
+    libm_common_src_files += \
 	i387/fenv.c \
 	i387/s_scalbnl.S \
 	i387/s_scalbn.S \
@@ -187,18 +186,7 @@ ifeq ($(TARGET_OS)-$(TARGET_ARCH),linux-x86)
     endif
   endif
 endif
-ifeq ($(TARGET_ARCH),mips)
-  libm_common_src_files += \
-	mips/fenv.c \
-	src/e_ldexpf.c \
-	src/s_scalbln.c \
-	src/s_scalbn.c \
-	src/s_scalbnf.c
 
-  libm_common_includes = $(LOCAL_PATH)/mips
-  # Need to build *rint* functions
-  libm_common_cflags += -fno-builtin-rintf -fno-builtin-rint
-endif
 
 # libm.a
 # ========================================================
@@ -210,7 +198,6 @@ LOCAL_SRC_FILES := \
 
 LOCAL_ARM_MODE := arm
 LOCAL_C_INCLUDES += $(libm_common_includes)
-LOCAL_CFLAGS := $(libm_common_cflags)
 
 LOCAL_MODULE:= libm
 
@@ -229,7 +216,6 @@ LOCAL_SRC_FILES := \
 LOCAL_ARM_MODE := arm
 
 LOCAL_C_INCLUDES += $(libm_common_includes)
-LOCAL_CFLAGS := $(libm_common_cflags)
 
 LOCAL_MODULE:= libm
 
